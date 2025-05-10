791ba3767cf97565b552d8582dc87bb7673817f4
yu_xianglong <yu_xianglong@qq.com>
2025-05-10 14:16:02 +0800

    demo+

--- hello_kabuda_vue2/src/App.vue
+++ hello_kabuda_vue2/src/App.vue
@@ -1,25 +1,80 @@
 <template>
-  <div id="app">
-    <Count />
-    <hr>
-    <Person />
-  </div>
+    <div id="root">
+        <div class="todo-container">
+            <div class="todo-wrap">
+                <MyHeader />
+                <MyList :todos="todos" />
+                <MyFooter />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Count from './components/count.vue'
-import Person from './components/person.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Count,
-    Person
-  },
-  mounted() {
-    console.log('App', this)
-  }
-}
+    import MyHeader from './components/MyHeader'
+    import MyList from './components/MyList'
+    import MyFooter from './components/MyFooter.vue'
+    export default {
+        name: 'App',
+        components: { MyHeader, MyList, MyFooter },
+        data() {
+            return {
+                todos: [
+                { id: '001', title: 'A', done: true },
+                { id: '002', title: 'B', done: false },
+                { id: '003', title: 'C', done: true }]
+            }
+        },
+        mounted() {
+            console.log('App', this)
+        }
+    }
 </script>
 
-<style></style>
+<style>
+
+    /*base*/
+    body {
+        background: #fff;
+    }
+
+    .btn {
+        display: inline-block;
+        padding: 4px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+        line-height: 20px;
+        text-align: center;
+        vertical-align: middle;
+        cursor: pointer;
+        box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+        border-radius: 4px;
+    }
+
+    .btn-danger {
+        color: #fff;
+        background-color: #da4f49;
+        border: 1px solid #bd362f;
+    }
+
+        .btn-danger:hover {
+            color: #fff;
+            background-color: #bd362f;
+        }
+
+    .btn:focus {
+        outline: none;
+    }
+
+    .todo-container {
+        width: 600px;
+        margin: 0 auto;
+    }
+
+        .todo-container .todo-wrap {
+            padding: 10px;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+        }
+</style>
+

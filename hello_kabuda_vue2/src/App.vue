93121fd053998294314b67c19b2a5ef9574f0df6
yu_xianglong <yu_xianglong@qq.com>
2025-05-16 15:25:18 +0800

    自定义事件

--- hello_kabuda_vue2/src/App.vue
+++ hello_kabuda_vue2/src/App.vue
@@ -1,121 +1,55 @@
 <template>
-    <div id="root">
-        <div class="todo-container">
-            <div class="todo-wrap">
-                <MyHeader :addTodoItem="addTodoItem" @addTodo="addTodoItem" />
-                <MyList :todos="todos" :checkTodo="checkTodo" :delTodo="delTodo" />
-                <MyFooter :todos="todos" :allTodo="allTodo" :clearAllTodo="clearAllTodo" />
-            </div>
-        </div>
+    <div class="app">
+        <h1>{{msg}}，学生姓名是:{{studentName}}</h1>
+
+        <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
+        <School :getSchoolName="getSchoolName" />
+
+        <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第一种写法，使用@或v-on） -->
+         <Student @atguigu="getStudentName" @demo="m1"/> 
+        <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第二种写法，使用ref） -->
+        <!--<Student ref="student" @click.native="show" />-->
     </div>
 </template>
 
 <script>
-import MyHeader from './components/MyHeader'
-import MyList from './components/MyList'
-import MyFooter from './components/MyFooter.vue'
-export default {
-    name: 'App',
-    components: { MyHeader, MyList, MyFooter },
-    data() {
-        return {
-            // todos: [
-            // { id: '001', title: '工作', done: true },
-            // { id: '002', title: '学习', done: false },
-            // { id: '003', title: '开车', done: true }
-            // ]
+    import Student from './components/Student'
+    import School from './components/School'
 
-            todos: JSON.parse(localStorage.getItem('todos')) || []
-        }
-    },
-    methods: {
-        // 添加todo
-        addTodoItem(item) {
-            this.todos.unshift(item);
-        },
-        // 选择todo 
-        checkTodo(id) {
-            this.todos.forEach((todo) => {
-                if (todo.id === id) {
-                    todo.done = !todo.done
-                    console.log(todo)
-                }
-            })
-        },
-        // 删除todo 
-        delTodo(id) {
-            this.todos = this.todos.filter(todo => todo.id !== id)
-        },
-        allTodo(done) {
-            this.todos.forEach((todo) => {
-                todo.done = done
-            })
-        },
-        clearAllTodo() {
-
-            this.todos = this.todos.filter((todo) => {
-                return !todo.done
-            })
-        }
-    },
-    watch: {
-        todos: {
-            deep: true,
-            handler(value) {
-                localStorage.setItem('todos', JSON.stringify(value))
+    export default {
+        name: 'App',
+        components: { School, Student },
+        data() {
+            return {
+                msg: '你好啊！',
+                studentName: ''
             }
-        }
-
-    },
-    mounted() {
-        console.log('App', this)
-    },
-
-}
+        },
+        methods: {
+            getSchoolName(name) {
+                console.log('App收到了学校名：', name)
+            },
+            getStudentName(name, ...params) {
+                console.log('App收到了学生名：', name, params)
+                this.studentName = name
+            },
+            m1() {
+                console.log('demo事件被触发了！')
+            },
+            show() {
+                alert(123)
+            }
+        },
+        mounted() {
+            this.$refs.student.$on('atguigu', this.getStudentName) //绑定自定义事件
+            // this.$refs.student.$once('atguigu',this.getStudentName) //绑定自定义事件（一次性）
+        },
+    }
 </script>
 
-<style>
-/*base*/
-body {
-    background: #fff;
-}
-
-.btn {
-    display: inline-block;
-    padding: 4px 12px;
-    margin-bottom: 0;
-    font-size: 14px;
-    line-height: 20px;
-    text-align: center;
-    vertical-align: middle;
-    cursor: pointer;
-    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-    border-radius: 4px;
-}
-
-.btn-danger {
-    color: #fff;
-    background-color: #da4f49;
-    border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-    color: #fff;
-    background-color: #bd362f;
-}
-
-.btn:focus {
-    outline: none;
-}
-
-.todo-container {
-    width: 600px;
-    margin: 0 auto;
-}
-
-.todo-container .todo-wrap {
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-}
+<style scoped>
+    .app {
+        background-color: gray;
+        padding: 5px;
+    }
 </style>
--- hello_kabuda_vue2/src/main.js
+++ hello_kabuda_vue2/src/main.js
@@ -1,10 +1,17 @@
+//引入Vue
 import Vue from 'vue'
+//引入App
 import App from './App.vue'
-import store from './store'
+//关闭Vue的生产提示
 Vue.config.productionTip = false
 
+//创建vm
 new Vue({
-  store,
-  render: h => h(App),
-}).$mount('#app')
-
+	el: '#app',
+	render: h => h(App),
+	/* mounted() {
+		setTimeout(()=>{
+			this.$destroy()
+		},3000)
+	}, */
+})
